72aa516a57ae536af65b4efbc062c55a95a43bf8
Grégoire Détrez <gdetrez@crans.org>
2012-06-23 15:24:30 +0200

    Deployment using gunicorn

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,9 @@ import os
 env.hosts = ['178.79.156.98']
 env.user = 'gdetrez'
 env.root = '/srv/members.ffkp.se'
+env.project_root = os.path.join(env.root,'ffkp')
 env.virtualenv_root = os.path.join(env.root,'env')
+env.activate_script = os.path.join(env.virtualenv_root, 'bin','activate')
 
 RSYNC_EXCLUDE = (
     '.DS_Store',
@@ -20,14 +22,38 @@ RSYNC_EXCLUDE = (
     'local_settings.py',
     'fabfile.py',
     'bootstrap.py',
+    '_static'
 )
 
 @task(default=True)
 def deploy():
-    """ rsync code to remote host """
     if not console.confirm('Are you sure you want to deploy production?',
                            default=False):
         utils.abort('Production deployment aborted.')
+    rsync()
+    collectstatic()
+    reload()
+
+
+def virtualenv(command, use_sudo=False):
+    if use_sudo:
+        func = sudo
+    else:
+        func = run
+    func('source "%s" && %s' % (env.activate_script, command))
+
+def manage_py(command, use_sudo=False):
+    with cd(env.project_root):
+        virtualenv('python manage.py %s' % command, use_sudo)
+
+@task
+def collectstatic():
+    #require('hosts', provided_by=[production])
+    manage_py('collectstatic -l --noinput')
+
+@task
+def rsync():
+    """ rsync code to remote host """
     extra_opts = '--omit-dir-times'
     rsync_project(
         env.root,
@@ -35,11 +61,8 @@ def deploy():
         delete=True,
         extra_opts=extra_opts,
     )
-    touch()
 
-def touch():
-    """ touch wsgi file to trigger reload """
-    #require('code_root', provided_by=('staging', 'production'))
-    apache_dir = os.path.join(env.root, 'ffkp', 'ffkp')
-    with cd(apache_dir):
-        run('touch wsgi.py')
+@task
+def reload():
+    """ Restart the wsgi process """
+    sudo("supervisorctl restart members.ffkp.se")
--- ffkp/wsgi.py
+++ ffkp/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ffkp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ffkp.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
